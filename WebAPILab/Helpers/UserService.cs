71e804a7e9344eb6345bbb9f9c31690ff273f73e
rodrigo <rodrigoalesm@gmail.com>
2020-02-19 17:12:42 -0600

    getUsersCambios

--- WebAPILab/Helpers/UserService.cs
+++ WebAPILab/Helpers/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.Extensions.Options;
 using WebAPILab.Models;
 
@@ -14,16 +15,41 @@ namespace WebAPILab.Helpers
 
     public class UserService : IUserService
     {
-        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
-        private List<UserModel> _users = new List<UserModel>
+        public IList<UserModel> loadUsers()
         {
-            new UserModel { Id = 1, Name = "Rodri", FirstName = "Alejandro", SecondName = "Soto", Address = "50m oeste escuela Sta Lucía",
-                Phone= "98769485", SecondContact="None", Email="rodrigoalesm@gmail.com", Password="rodri123" }
-        };
+            IList<UserModel> _users = null;
+
+            using (var context = new GARSupport2020Entities())
+            {
+                _users = context.Users
+                    .Select(userItem => new UserModel()
+                    {
+                        Id = userItem.id,
+                        Name = userItem.name,
+                        FirstName = userItem.first_name,
+                        SecondName = userItem.second_name,
+                        Address = userItem.address,
+                        Phone = userItem.phone,
+                        SecondContact = userItem.second_contact,
+                        Email = userItem.email,
+                        Password = userItem.password
+
+                    }).ToList<UserModel>();
+                if (_users.Count == 0)
+                {
+                    return null;
+                }
+                return _users;
+            }
+        }
+        
+        //private List<UserModel> _users = new List<UserModel>
+        //{
+        //};
 
         public async Task<UserModel> Authenticate(string email, string password)
         {
-            var user = await Task.Run(() => _users.SingleOrDefault(x => x.Email == email && x.Password == password));
+            var user = await Task.Run(() => loadUsers().SingleOrDefault(x => x.Email == email && x.Password == password));
 
             // return null if user not found
             if (user == null)
@@ -37,7 +63,7 @@ namespace WebAPILab.Helpers
         public async Task<IEnumerable<UserModel>> GetAll()
         {
             // return users without passwords
-            return await Task.Run(() => _users.Select(x => {
+            return await Task.Run(() => loadUsers().Select(x => {
                 x.Password = null;
                 return x;
             }));
