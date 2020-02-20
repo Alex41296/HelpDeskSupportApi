799e867f56cd075a1410999e63c12e0b3d2192d5
Alex41296 <alexfer41296@gmail.com>
2020-02-19 18:01:15 -0600

    Se agrego el controller y modelo para asignar

---
+++ WebAPILab/Controllers/UsersIssueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPILab.Models;
+
+namespace WebAPILab.Controllers
+{
+    public class UsersIssueController : ApiController
+    {
+        public IHttpActionResult Post(UsersIssueModel usersIssue)
+        {
+            using (var context = new GARSupport2020Entities())
+            {
+                context.Users_Issue.Add(new Users_Issue()
+                {
+                    id = usersIssue.Id,
+                    id_users = usersIssue.Id_Users,
+                    id_issue = usersIssue.Id_Users
+                });
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ WebAPILab/Models/UsersIssueModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPILab.Models
+{
+    public class UsersIssueModel
+    {
+        public int Id { get; set; }
+        public Nullable<int> Id_Users { get; set; }
+        public Nullable<int> Id_Issue { get; set; }
+
+    }
+}
